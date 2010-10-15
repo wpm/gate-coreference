973f7b106137edc861370b1f12461f2deba964f8
W.P. McNeill <billmcn@gmail.com>
2010-10-15 07:03:40 -0700

    Add Logging to Coreference Viewer
    Currently set to info level.

--- src/gate/coreference/CoreferenceScoringViewer.java
+++ src/gate/coreference/CoreferenceScoringViewer.java
@@ -1,5 +1,7 @@
 package gate.coreference;
 
+import org.apache.log4j.Logger;
+
 import gate.Resource;
 import gate.creole.AbstractVisualResource;
 import gate.creole.ResourceInstantiationException;
@@ -13,39 +15,50 @@ import gate.event.CorpusListener;
  */
 @SuppressWarnings("serial")
 @CreoleResource(name = "Corpus Coreference Score", guiType = GuiType.LARGE,
-	    resourceDisplayed = "gate.Corpus", mainViewer = false)
+		resourceDisplayed = "gate.Corpus", mainViewer = false)
 public class CoreferenceScoringViewer extends AbstractVisualResource implements
 		CorpusListener {
 
+	static Logger logger = Logger.getLogger(CoreferenceScoringViewer.class
+			.getName());
+
 	public Resource init() throws ResourceInstantiationException {
+		logger.info("Initialize");
 		return super.init();
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see gate.creole.AbstractVisualResource#setTarget(java.lang.Object)
 	 */
 	@Override
 	public void setTarget(Object target) {
 		// TODO Auto-generated method stub
+		logger.info("Set target " + target.toString());
 		super.setTarget(target);
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see gate.event.CorpusListener#documentAdded(gate.event.CorpusEvent)
 	 */
 	@Override
 	public void documentAdded(CorpusEvent e) {
 		// TODO Auto-generated method stub
-
+		logger.info("Document added " + e.toString());
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see gate.event.CorpusListener#documentRemoved(gate.event.CorpusEvent)
 	 */
 	@Override
 	public void documentRemoved(CorpusEvent e) {
 		// TODO Auto-generated method stub
-
+		logger.info("Document removed " + e.toString());
 	}
 
 }
